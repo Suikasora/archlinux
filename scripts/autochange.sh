52271a2c10ded0ea8ac4203719111890ee4fc5f5
Suikasora <S_Aihui@outlook.com>
2020-03-01 04:22:48 +0800

    1

---
+++ scripts/autochange.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+while true; do
+/bin/bash /home/s/scripts/change.sh
+	sleep 3m
+done
---
+++ scripts/change.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+feh --recursive --randomize --bg-fill /home/s/Downloads/Wallpaper
+
